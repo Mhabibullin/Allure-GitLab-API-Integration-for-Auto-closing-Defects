4f9f02e01efddf74c8a557b2f56ca481bc386de1
Max Khabibullin <habibullinmax@gmail.com>
2023-05-21 14:18:56 +0500

    fixes

--- src/main/java/mkhabibullin/core/ApiExecutionAllure.java
+++ src/main/java/mkhabibullin/core/ApiExecutionAllure.java
@@ -16,23 +16,21 @@ import java.util.Set;
 
 public class ApiExecutionAllure {
 
-    private String responseForGetRequest(HttpURLConnection connection) throws IOException {
-        StringBuilder response;
-        InputStream is;
-        if (connection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
-            is = connection.getInputStream();
-        } else {
-            is = connection.getErrorStream();
+    private static void allowMethods(String... methods) {
+        try {
+            Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(methodsField, methodsField.getModifiers() & ~Modifier.FINAL);
+            methodsField.setAccessible(true);
+            String[] oldMethods = (String[]) methodsField.get(null);
+            Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
+            methodsSet.addAll(Arrays.asList(methods));
+            String[] newMethods = methodsSet.toArray(new String[0]);
+            methodsField.set(null, newMethods);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new IllegalStateException(e);
         }
-        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-        response = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            response.append(line);
-            response.append(System.lineSeparator());
-        }
-        rd.close();
-        return response.toString();
     }
 
     public String executeGetJson(String targetURL,
@@ -51,12 +49,12 @@ public class ApiExecutionAllure {
             connection.setRequestMethod("GET");
             connection.setUseCaches(false);
             connection.setDoOutput(true);
-            if (header != null && ! header.getHeader().isEmpty()) {
+            if (header != null && !header.getHeader().isEmpty()) {
                 for (Map.Entry<String, String> entry : header.getHeader().entrySet()) {
                     connection.addRequestProperty(entry.getKey(), entry.getValue());
                 }
             }
-            response = responseForGetRequest(connection);
+            response = readResponseForEmptyBodyRequest(connection);
         } catch (Exception e) {
             if (connection == null) {
                 throw new AssertionError();
@@ -70,7 +68,65 @@ public class ApiExecutionAllure {
         return response;
     }
 
-    private String readResponse(HttpURLConnection connection, String body) throws IOException {
+
+    public String executePatchJson(String targetURL,
+                                   @Nullable String urlParameters,
+                                   @Nullable Header header,
+                                   String body) throws Exception {
+        HttpURLConnection connection = null;
+        StringBuilder stringBuilder = new StringBuilder(targetURL);
+        String response;
+        if (urlParameters != null) {
+            stringBuilder.append("?");
+            stringBuilder.append(urlParameters);
+        }
+        try {
+            allowMethods("PATCH");
+            URL url = new URL(stringBuilder.toString());
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("PATCH");
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+            connection.addRequestProperty("Content-Type", "application/json");
+            if (header != null && !header.getHeader().isEmpty()) {
+                for (Map.Entry<String, String> entry : header.getHeader().entrySet()) {
+                    connection.addRequestProperty(entry.getKey(), entry.getValue());
+                }
+            }
+            response = readResponseForRequestWithBody(connection, body);
+        } catch (Exception e) {
+            if (connection == null) {
+                throw new AssertionError();
+            }
+            throw new Exception(connection.getResponseMessage());
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return response;
+    }
+
+    private String readResponseForEmptyBodyRequest(HttpURLConnection connection) throws IOException {
+        StringBuilder response;
+        InputStream is;
+        if (connection.getResponseCode() < HttpURLConnection.HTTP_BAD_REQUEST) {
+            is = connection.getInputStream();
+        } else {
+            is = connection.getErrorStream();
+        }
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        response = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            response.append(line);
+            response.append(System.lineSeparator());
+        }
+        rd.close();
+        return response.toString();
+    }
+
+    private String readResponseForRequestWithBody(HttpURLConnection connection, String body) throws IOException {
         StringBuilder response;
         InputStream is;
         try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
@@ -107,61 +163,6 @@ public class ApiExecutionAllure {
             }
         }
         return json;
-    }
-
-    private static void allowMethods(String... methods) {
-        try {
-            Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
-            Field modifiersField = Field.class.getDeclaredField("modifiers");
-            modifiersField.setAccessible(true);
-            modifiersField.setInt(methodsField, methodsField.getModifiers() & ~ Modifier.FINAL);
-            methodsField.setAccessible(true);
-            String[] oldMethods = (String[]) methodsField.get(null);
-            Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
-            methodsSet.addAll(Arrays.asList(methods));
-            String[] newMethods = methodsSet.toArray(new String[0]);
-            methodsField.set(null, newMethods);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
-    }
-
-    public String executePatchJson(String targetURL,
-                                   @Nullable String urlParameters,
-                                   @Nullable Header header,
-                                   String body) throws Exception {
-        HttpURLConnection connection = null;
-        StringBuilder stringBuilder = new StringBuilder(targetURL);
-        String response;
-        if (urlParameters != null) {
-            stringBuilder.append("?");
-            stringBuilder.append(urlParameters);
-        }
-        try {
-            allowMethods("PATCH");
-            URL url = new URL(stringBuilder.toString());
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("PATCH");
-            connection.setUseCaches(false);
-            connection.setDoOutput(true);
-            connection.addRequestProperty("Content-Type", "application/json");
-            if (header != null && ! header.getHeader().isEmpty()) {
-                for (Map.Entry<String, String> entry : header.getHeader().entrySet()) {
-                    connection.addRequestProperty(entry.getKey(), entry.getValue());
-                }
-            }
-            response = readResponse(connection, body);
-        } catch (Exception e) {
-            if (connection == null) {
-                throw new AssertionError();
-            }
-            throw new Exception(connection.getResponseMessage());
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-        return response;
     }
 
 }
